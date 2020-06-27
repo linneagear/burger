3b278be87191c7811a89e13a9bcf8aa7c8e54b09
Linnea <linneagear@gmail.com>
2020-06-27 15:39:25 -0700

    Added three functions to model>burger.js and exported object

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,15 +7,16 @@ const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    burger.all(function(data)) {
+    burger.all(function(data) {
+        // grab the burger data from the schema
         const burgerObj = {
             burgers: data
         };
         console.log(burgerObj);
-        
+
         // render your burger object (data) to the index file
         res.render("index", burgerObj);
-    }
+    });
 });
 
 router.post("/api/burgers", function(req, res) {
@@ -24,7 +25,7 @@ router.post("/api/burgers", function(req, res) {
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-
+    burger.create
 
 });
 
--- models/burger.js
+++ models/burger.js
@@ -4,9 +4,23 @@ const orm = require("../config/orm.js");
 // call the ORM functions using burger specific input for the ORM
 const burger = {
 
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
 
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
 
-
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res)
+        });
+    }
 };
 
 // Export the database functions for the controller.
