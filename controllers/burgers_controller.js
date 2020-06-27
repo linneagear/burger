6902f4abab9967bbf2de655e05411418a2a7b5d8
Linnea <linneagear@gmail.com>
2020-06-26 19:42:39 -0700

    Adding image and css styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,10 +6,25 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+
+});
+
+router.post("/api/burgers", function(req, res) {
 
 
+});
+
+router.put("/api/burgers/:id", function(req, res) {
 
 
+});
+
+
+router.delete("/api/burger/:id", function(req, res) {
+
+
+});
 
 
 // Export routes for server.js to use.
