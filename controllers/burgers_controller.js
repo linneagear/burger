a0231d315a4bb87d8cb0dfeee374d0dbb20cf55b
Linnea <linneagear@gmail.com>
2020-06-27 15:46:59 -0700

    Completed routes and added standard server setup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,9 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-        // grab the burger data from the schema
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        // grab the burger data
         const burgerObj = {
             burgers: data
         };
@@ -19,20 +19,31 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post("/api/burgers", function(req, res) {
-
-
+// post data to API
+router.post("/api/burgers", function (req, res) {
+    // create the new object
+    burger.insertOne(["name", "devoured"], [req.body.name, req.query.devoured], function (result) {
+        res.json({ id: result.insertId })
+    });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-    burger.create
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
 
-});
+    console.log("condition", condition);
 
-
-router.delete("/api/burger/:id", function(req, res) {
-
-
+    burger.updateOne(
+        {
+            devoured: req.query.devoured
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        });
 });
 
 
